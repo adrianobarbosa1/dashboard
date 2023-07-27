37f4479bce6fd99f52da3397071a03b78d4f41ae
Adriano Barbosa <adriano.barbosa100@email.com>
2023-07-27 00:32:19 -0300

    feat: move sidebar to active sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import SideBar from "@/components/SideBar";
+import ActiveSidebar from "@/components/ActiveSidebar";
+import { StateProvider } from "@/contexts/ContextProvider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -15,34 +16,14 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const activeManu = {};
   return (
     <html lang="pt-BR">
       <body className={inter.className}>
-        {/* SIDEBAR */}
-        {activeManu ? (
-          <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-            <SideBar />
-          </div>
-        ) : (
-          <div className="w-0 dark:bg-secondary-dark-bg">
-            <SideBar />
-          </div>
-        )}
+        <StateProvider>
+          {/* SIDEBAR */}
 
-        {/* HEADER */}
-        <div
-          className={`dark:bg-main-bg bg-main-bg min-h-screen w-full 
-        ${activeManu ? "md:ml-72" : "flex-2"}
-        `}
-        >
-          <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-            <h1>navbar</h1>
-          </div>
-        </div>
-
-        {/* {PAGE} */}
-        {children}
+          <ActiveSidebar>{children}</ActiveSidebar>
+        </StateProvider>
       </body>
     </html>
   );
