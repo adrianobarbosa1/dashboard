b011fa6e5fd75c98cf3d4146b83d31e52f563520
Adriano Barbosa <adriano.barbosa100@email.com>
2023-07-27 00:33:18 -0300

    feat: add sidebar

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,24 +1,29 @@
+"use client";
+import { useStateContext } from "@/contexts/ContextProvider";
 import { links } from "@/data/data/dummy";
 import Link from "next/link";
 import { MdOutlineCancel } from "react-icons/md";
 import { SiShopware } from "react-icons/si";
 
 const SideBar = () => {
-  const activeMenu = true;
+  const { activeMenu, setActiveMenu } = useStateContext();
+
   return (
-    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+    <div className=" ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
         <>
-          <div className=" flex justify-between items-center">
+          <div className="flex justify-between items-center">
             <Link
               href="/"
-              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tighter dark:text-white text-slate-900"
+              onClick={() => setActiveMenu(false)}
+              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
-              <SiShopware /> <span>Shoppy</span>
+              <SiShopware /> <span>Logo</span>
             </Link>
 
             <button
               type="button"
+              onClick={() => setActiveMenu(!activeMenu)}
               className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
             >
               <MdOutlineCancel />
@@ -31,6 +36,24 @@ const SideBar = () => {
                 <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
                   {item.title}
                 </p>
+                <ul>
+                  {item.links.map((link) => (
+                    <Link
+                      href={link.name}
+                      key={link.name}
+                      className={`flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg m-2 ${({
+                        isActive,
+                      }) =>
+                        isActive
+                          ? "text-white  text-md"
+                          : "text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray"}
+                      `}
+                    >
+                      {link.icon}
+                      <span className="capitalize">{link.name}</span>
+                    </Link>
+                  ))}
+                </ul>
               </div>
             ))}
           </div>
