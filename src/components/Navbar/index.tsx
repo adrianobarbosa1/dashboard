7511c4879f14394dee62627c401f7b4684908976
Adriano Barbosa <adriano.barbosa100@email.com>
2023-07-27 00:32:54 -0300

    feat: add navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,81 @@
+"use client";
+import { useStateContext } from "@/contexts/ContextProvider";
+import Image from "next/image";
+import { AiOutlineMenu } from "react-icons/ai";
+import { BsChatLeft } from "react-icons/bs";
+import { FiShoppingCart } from "react-icons/fi";
+import { RiNotification3Line } from "react-icons/ri";
+import Tooltip from "../Tooltip";
+import NavButton from "./NavButton";
+
+import { MdKeyboardArrowDown } from "react-icons/md";
+
+const Navbar = () => {
+  const { activeMenu, setActiveMenu, handleClick, isClicked } =
+    useStateContext();
+
+  const handleActiveMenu = () => setActiveMenu(!activeMenu);
+
+  return (
+    <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
+      <NavButton
+        title="Menu"
+        customFunc={handleActiveMenu}
+        color="blue"
+        icon={<AiOutlineMenu />}
+      />
+
+      <div className="flex">
+        <NavButton
+          title="Cart"
+          customFunc={() => handleClick("cart")}
+          color="blue"
+          icon={<FiShoppingCart />}
+        />
+        <NavButton
+          title="Chat"
+          dotColor="#03c9D7"
+          customFunc={() => handleClick("Chat")}
+          color="blue"
+          icon={<BsChatLeft />}
+        />
+        <NavButton
+          title="Notifications"
+          dotColor="#03c9D7"
+          customFunc={() => handleClick("notifications")}
+          color="blue"
+          icon={<RiNotification3Line />}
+        />
+
+        <Tooltip content="Profile">
+          <div
+            onClick={() => handleClick("userProfile")}
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+          >
+            <Image
+              src="/avatar.jpg"
+              width={32}
+              height={32}
+              alt="user-profile"
+              className="rounded-full"
+            />
+            <p>
+              <span className="text-gray-400 text-14">Hi,</span>{" "}
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                Michael
+              </span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
+          </div>
+        </Tooltip>
+
+        {isClicked.cart && "<Cart />"}
+        {isClicked.chat && "<Chat />"}
+        {isClicked.notification && "<Notification />"}
+        {isClicked.userProfile && "<UserProfile />"}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
