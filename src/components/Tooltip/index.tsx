2682d9a80f069092c3a190ff9db28e424f22f6ad
Adriano Barbosa <adriano.barbosa100@email.com>
2023-07-31 11:12:09 -0300

    feat: create tooltip

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useState } from "react";
 
 type TooltipProps = {
   content: string;
@@ -7,21 +6,14 @@ type TooltipProps = {
 };
 
 const Tooltip = ({ content, children }: TooltipProps) => {
-  const [show, setShow] = useState(false);
-
   return (
-    <div
-      className="relative inline-block"
-      onMouseEnter={() => setShow(true)}
-      onMouseLeave={() => setShow(false)}
-    >
-      {children}
-
-      {show && (
-        <div className="absolute opacity-0 w-28 bg-black text-white text-center text-xs rounded-lg py-2 z-10 group-hover:opacity-100 bottom-full -left-1/2 ml-14 px-3 pointer-events-none">
+    <div className="relative inline-block">
+      <div className="group">
+        {children}
+        <div className="hidden group-hover:block bg-black/50 text-white text-xs rounded py-1 px-2 absolute left-1/2 transform -translate-x-1/2">
           {content}
         </div>
-      )}
+      </div>
     </div>
   );
 };
