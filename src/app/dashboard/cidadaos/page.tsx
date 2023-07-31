964c02070e0cd641a6516de0e067b5f174b2cca3
Adriano Barbosa <adriano.barbosa100@email.com>
2023-07-31 10:56:07 -0300

    feat: add page cidadao

---
+++ src/app/dashboard/cidadaos/page.tsx
@@ -0,0 +1,11 @@
+import Card from "@/components/Card";
+
+const page = () => {
+  return (
+    <Card category="Page" title="Cidadãos">
+      <div>texto</div>
+    </Card>
+  );
+};
+
+export default page;
