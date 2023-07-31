66940ea45b29e3c6eb24b933f07a6000c979c713
Adriano Barbosa <adriano.barbosa100@email.com>
2023-07-31 11:02:48 -0300

    feat: add page servidores

---
+++ src/app/dashboard/servidores/page.tsx
@@ -0,0 +1,11 @@
+import Card from "@/components/Card";
+
+const page = () => {
+  return (
+    <Card category="Page" title="Servidores">
+      <div>texto</div>
+    </Card>
+  );
+};
+
+export default page;
