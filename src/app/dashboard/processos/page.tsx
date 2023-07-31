a8a5e8088d294829eb653a6b7b17404c82260dff
Adriano Barbosa <adriano.barbosa100@email.com>
2023-07-31 11:02:34 -0300

    feat: add pages processos

---
+++ src/app/dashboard/processos/page.tsx
@@ -0,0 +1,11 @@
+import Card from "@/components/Card";
+
+const page = () => {
+  return (
+    <Card category="Page" title="Processos">
+      <div>texto</div>
+    </Card>
+  );
+};
+
+export default page;
