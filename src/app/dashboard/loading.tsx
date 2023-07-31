99080393d4a129338dbe3dd5233395070003a179
Adriano Barbosa <adriano.barbosa100@email.com>
2023-07-31 10:55:14 -0300

    feat: create component loading

---
+++ src/app/dashboard/loading.tsx
@@ -0,0 +1,12 @@
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div
+        className="w-12 h-12 rounded-full animate-spin
+border-y-2 border-solid border-blue-500 border-t-transparent"
+      />
+    </div>
+  );
+};
+
+export default Loading;
