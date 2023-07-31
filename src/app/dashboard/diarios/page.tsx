5c788e6db8131cbed6dd5a2352bccf9fd9f8697d
Adriano Barbosa <adriano.barbosa100@email.com>
2023-07-31 10:56:19 -0300

    feat: add page diarios

---
+++ src/app/dashboard/diarios/page.tsx
@@ -0,0 +1,12 @@
+import Card from "@/components/Card";
+import Table from "@/components/Table";
+
+const page = () => {
+  return (
+    <Card category="Page" title="Diários">
+      <Table />
+    </Card>
+  );
+};
+
+export default page;
