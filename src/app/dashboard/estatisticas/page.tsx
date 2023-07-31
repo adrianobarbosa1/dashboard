0e27d3fc8170fb5e1d0b301ebd5922cdcfb5cb18
Adriano Barbosa <adriano.barbosa100@email.com>
2023-07-31 11:02:17 -0300

    feat: add page estatisticas

---
+++ src/app/dashboard/estatisticas/page.tsx
@@ -0,0 +1,11 @@
+import Card from "@/components/Card";
+
+const page = () => {
+  return (
+    <Card category="Page" title="Estatísticas">
+      <div>texto</div>
+    </Card>
+  );
+};
+
+export default page;
