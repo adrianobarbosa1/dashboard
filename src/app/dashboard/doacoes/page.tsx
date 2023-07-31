4a86542befc5c8a7e2d7d8ab7a6a1f75f9389779
Adriano Barbosa <adriano.barbosa100@email.com>
2023-07-31 11:00:42 -0300

    feat: add page doacoes

---
+++ src/app/dashboard/doacoes/page.tsx
@@ -0,0 +1,11 @@
+import Card from "@/components/Card";
+
+const page = () => {
+  return (
+    <Card category="Page" title="Doações">
+      <div>texto</div>
+    </Card>
+  );
+};
+
+export default page;
