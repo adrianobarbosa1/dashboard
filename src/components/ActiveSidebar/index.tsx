c9bee3e71ef84e3003e2491443407bed6ba4516a
Adriano Barbosa <adriano.barbosa100@email.com>
2023-07-31 11:03:30 -0300

    feat: add sidebar logica

--- src/components/ActiveSidebar/index.tsx
+++ src/components/ActiveSidebar/index.tsx
@@ -1,48 +1,76 @@
 "use client";
 import { useStateContext } from "@/contexts/ContextProvider";
+import { useEffect } from "react";
 import { FiSettings } from "react-icons/fi";
 import Navbar from "../Navbar";
 import SideBar from "../SideBar";
+import ThemeSettings from "../ThemeSettings";
 
 type ActiveSidebarProps = {
   children: React.ReactNode;
 };
 
 const ActiveSidebar = ({ children }: ActiveSidebarProps) => {
-  const { activeMenu } = useStateContext();
+  const {
+    activeMenu,
+    themeSettings,
+    setThemeSettings,
+    currentColor,
+    currentMode,
+    setCurrentColor,
+    setCurrentMode,
+  } = useStateContext();
+
+  useEffect(() => {
+    const currentThemeColor = localStorage.getItem("colorMode");
+    const currentThemeMode = localStorage.getItem("themeMode");
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
+    }
+  }, []);
+
   return (
-    <div className="flex relative dark:bg-main-dark-bg">
-      <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
-        <button
-          type="button"
-          // onClick={() => setThemeSettings(true)}
-          // style={{ background: currentColor, borderRadius: "50%" }}
-          className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+    <div className={currentMode === "Dark" ? "dark" : ""}>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+          <button
+            type="button"
+            onClick={() => setThemeSettings(true)}
+            style={{ background: currentColor, borderRadius: "50%" }}
+            className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+          >
+            <FiSettings />
+          </button>
+        </div>
+        {activeMenu ? (
+          <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white transition-all duration-300">
+            <SideBar />
+          </div>
+        ) : (
+          <div className="w-0 dark:bg-secondary-dark-bg transition-all duration-300">
+            <SideBar />
+          </div>
+        )}
+
+        {themeSettings && <ThemeSettings />}
+
+        <div
+          className={
+            activeMenu
+              ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+              : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+          }
         >
-          <FiSettings />
-        </button>
-      </div>
-      {activeMenu ? (
-        <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-          <SideBar />
+          <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+            <Navbar />
+          </div>
+
+          <div>
+            {/* {PAGE} */}
+            {children}
+          </div>
         </div>
-      ) : (
-        <div className="w-0 dark:bg-secondary-dark-bg">
-          <SideBar />
-        </div>
-      )}
-      <div
-        className={
-          activeMenu
-            ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
-            : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
-        }
-      >
-        <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
-          <Navbar />
-        </div>
-        {/* {PAGE} */}
-        {children}
       </div>
     </div>
   );
