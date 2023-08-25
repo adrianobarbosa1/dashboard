9ab9fcc0b8e310988d5c2b679749d2069924072d
Adriano Barbosa <adriano.barbosa100@email.com>
2023-08-25 16:07:54 -0300

    feat: add dropdownmenu

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,19 +1,20 @@
 "use client";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { useStateContext } from "@/contexts/ContextProvider";
 import Image from "next/image";
-import { AiOutlineMenu } from "react-icons/ai";
-import { BsChatLeft } from "react-icons/bs";
-import { FiShoppingCart } from "react-icons/fi";
-import { RiNotification3Line } from "react-icons/ri";
-import Tooltip from "../Tooltip";
-import NavButton from "./NavButton";
-
 import { useEffect } from "react";
-import { MdKeyboardArrowDown } from "react-icons/md";
-import Cart from "../Cart";
-import Chat from "../Chat";
-import Notification from "../Notification";
-import UserProfile from "../UserProfile";
+import { AiOutlineMenu, AiOutlineSetting } from "react-icons/ai";
+import { MdKeyboardArrowDown, MdPermIdentity } from "react-icons/md";
+import { RiLogoutCircleLine } from "react-icons/ri";
+import NavButton from "./NavButton";
 
 const Navbar = () => {
   const {
@@ -44,7 +45,7 @@ const Navbar = () => {
   const handleActiveMenu = () => setActiveMenu(!activeMenu);
 
   return (
-    <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
+    <div className=" flex justify-between p-2 md:ml-6 md:mr-6 relative">
       <NavButton
         title="Menu"
         customFunc={handleActiveMenu}
@@ -53,7 +54,7 @@ const Navbar = () => {
       />
 
       <div className="flex">
-        <NavButton
+        {/* <NavButton
           title="Cart"
           customFunc={() => handleClick("cart")}
           color={currentColor}
@@ -72,34 +73,66 @@ const Navbar = () => {
           customFunc={() => handleClick("notifications")}
           color={currentColor}
           icon={<RiNotification3Line />}
-        />
+        /> */}
 
-        <Tooltip content="Profile">
-          <div
-            onClick={() => handleClick("userProfile")}
-            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
-          >
-            <Image
-              src="/avatar.jpg"
-              width={32}
-              height={32}
-              alt="user-profile"
-              className="rounded-full"
-            />
-            <p>
-              <span className="text-gray-400 text-14">Hi,</span>{" "}
-              <span className="text-gray-400 font-bold ml-1 text-14">
-                Michael
-              </span>
-            </p>
-            <MdKeyboardArrowDown className="text-gray-400 text-14" />
-          </div>
-        </Tooltip>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <div
+              // onClick={() => handleClick("userProfile")}
+              className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            >
+              <Image
+                src="/avatar.jpg"
+                width={50}
+                height={50}
+                alt="user-profile"
+                className="rounded-full"
+              />
+              <p className="flex flex-col">
+                <span className="text-gray-400 font-bold ml-1 text-14">
+                  Adriano Barbosa dos Santos
+                </span>
+                <span className="text-green-400 font-bold ml-1 text-xs">
+                  conectado
+                </span>
+              </p>
+              <MdKeyboardArrowDown className="text-gray-400 text-14" />
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56" align="end" forceMount>
+            <DropdownMenuLabel className="font-normal">
+              <div className="flex flex-col space-y-1">
+                <p className="text-xs leading-none text-muted-foreground">
+                  adriano.barbosa100@gmail.com
+                </p>
+                <p className="text-xs leading-none text-muted-foreground">
+                  <span>Matricula: </span>27174
+                </p>
+                <p className="text-xs leading-none text-muted-foreground">
+                  admin
+                </p>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <MdPermIdentity className="mr-2 h-4 w-4" />
+                <span>Perfil</span>
+              </DropdownMenuItem>
 
-        {isClicked.cart && <Cart />}
-        {isClicked.chat && <Chat />}
-        {isClicked.notification && <Notification />}
-        {isClicked.userProfile && <UserProfile />}
+              <DropdownMenuItem>
+                <AiOutlineSetting className="mr-2 h-4 w-4" />
+                <span>Configuração</span>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+
+            <DropdownMenuItem>
+              <RiLogoutCircleLine className="mr-2 h-4 w-4" />
+              <span>Sair</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
