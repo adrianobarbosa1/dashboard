4d0f7d4fa01f2cc5ec7bd64eaaab50c30dc02df6
Breno Gomes da Fonseca <brenogfonseca@gmail.com>
2023-09-05 12:21:08 -0300

    DAS-13 adicionando toaster provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "tailwindcss/tailwind.css";
 import "./globals.css";
 import { Providers } from "@/lib/providers";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body className={inter.className}>
         <Providers>{children}</Providers>
+        <Toaster />
       </body>
     </html>
   );
