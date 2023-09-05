be6afba0d095a2d5ccf41581ab80abfe73afd62d
Breno Gomes da Fonseca <brenogfonseca@gmail.com>
2023-09-05 12:22:10 -0300

    DAS-13 configurando slice de usuario

--- src/lib/redux/slices/userSlice/thunks.ts
+++ src/lib/redux/slices/userSlice/thunks.ts
@@ -1,33 +1,16 @@
 /* Instruments */
 import { createAppAsyncThunk } from "@/lib/redux/createAppAsyncThunk";
-import { fetchIdentityCount } from "./fetchIdentityCount";
-import { selectUser } from "./selectors";
-import { counterSlice } from "./userSlice";
-import type { ReduxThunkAction } from "@/lib/redux";
+import { fetchLoginUser } from "./fetchLoginUser";
+import { LoginData } from "./user.types";
 
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched. Thunks are
-// typically used to make async requests.
-export const incrementAsync = createAppAsyncThunk(
-  "counter/fetchIdentityCount",
-  async (amount: number) => {
-    const response = await fetchIdentityCount(amount);
-
-    // The value we return becomes the `fulfilled` action payload
-    return response.data;
+export const loginUser = createAppAsyncThunk(
+  "user/fetchLoginUser",
+  async (loginData: LoginData, thunkApi) => {
+    const response = await fetchLoginUser(loginData);
+    console.log(response);
+    if (response.status === 500) {
+      return thunkApi.rejectWithValue(await response.json());
+    }
+    return response.json();
   }
 );
-
-// We can also write thunks by hand, which may contain both sync and async logic.
-// Here's an example of conditionally dispatching actions based on current state.
-export const incrementIfOddAsync =
-  (amount: number): ReduxThunkAction =>
-  (dispatch, getState) => {
-    const currentValue = selectCount(getState());
-
-    if (currentValue % 2 === 1) {
-      dispatch(counterSlice.actions.incrementByAmount(amount));
-    }
-  };
