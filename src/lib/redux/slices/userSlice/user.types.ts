d0c5d29be09297ca06210b0dc74dda6ee9f949b7
Breno Gomes da Fonseca <brenogfonseca@gmail.com>
2023-09-06 11:13:27 -0300

    DAS-13 tipando usuario

--- src/lib/redux/slices/userSlice/user.types.ts
+++ src/lib/redux/slices/userSlice/user.types.ts
@@ -2,3 +2,15 @@ export interface LoginData {
   username: string;
   password: string;
 }
+
+export interface User {
+  _id: string;
+  nome: string;
+  username: string;
+  password: string;
+  email: string;
+  permissao: string;
+  acesso: boolean;
+  dataCadastro: string;
+  __v: number;
+}
